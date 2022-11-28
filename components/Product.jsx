f7e956f7ae158b3471ddc2f55ccf698f8cf3f37e
cipher-preet <112680439+cipher-preet@users.noreply.github.com>
2022-11-28 17:54:14 +0530

    first commit

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({product :{image,name,slug,price}}) => {
     <div>
       <Link href={`/product/${slug.current}`}>
       <div className="product-card">
-        <img src={urlFor(image && image[0])}  width={250} height={250} className="product-image" />
+        <Image src={urlFor(image && image[0])}  width={250} height={250} className="product-image" />
         <p className="product-name">{name}</p>
         <p className='product-price'>${price} </p>
 
